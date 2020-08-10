6cebb9551e2d95e2b4c5111f3173ec3fc9b50843
Matt <matthew.eu.george@gmail.com>
2020-08-10 13:35:08 -0400

    first commit

---
+++ directiony.py
@@ -0,0 +1,7 @@
+#x is the actual direction while y is the ideal direction
+import math
+
+def directiony(x,y):
+            optimal = y
+            dir_radians = (math.cos((x - optimal)))
+            return(dir_radians)
